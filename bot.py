8ea017cc3388716789889b886ddd79afc4be6420
makefu <github@syntax-fehler.de>
2021-06-11 20:00:25 +0200

    prepare http api

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@
 
 """
 import asyncio
+import aiohttp
+import aiofiles as aiof
+
 from docopt import docopt
 from nio import AsyncClient
 import aiohttp
@@ -24,9 +27,28 @@ def color(text,color):
 def bold(text):
     return f"<b>{text}</b>"
 
-def format_data(obj):
+def format_data(obj,udpinput=False):
+    """ udpinput: set to True if the input arrived via UDP and not via HTTP
+    """
     print(obj)
     typ = obj['type']
+    if udpinput:
+        newrev = obj['revision']['new']
+        oldrev = obj['revision']['old']
+        ident = obj['id']
+        old_length = obj.get('length',{}).get('old',None)
+        new_length = obj.get('length',{}).get('new',None)
+        is_patrolled = obj['patrolled']
+        is_bot = obj['bot']
+    else:
+        newrev = obj['revid']
+        oldrev = obj['old_revid']
+        ident = obj['rcid']
+        old_length = obj.get('oldlen',None)
+        new_length = obj.get('newlen',None)
+        is_patrolled = False # does not work with http (need elevated permissions)
+        is_bot = False # does not provide the info
+
     if  typ == "log":
         title = f"Special:Log/{obj['log_type'].capitalize()}"
         url = ""
@@ -36,25 +58,22 @@ def format_data(obj):
         title = obj['title']
         comment = obj['comment']
         flag = ""
-        if obj['patrolled']:
+        if is_patrolled:
             flag += '!'
-
         if typ == "new":
-            query = f"?oldid={obj['revision']['new']}&rc_id={obj['id']}"
+            query = f"?oldid={newrev}&rc_id={ident}"
             flag += "N"
         else:
-            query = f"?diff={obj['revision']['new']}&oldid={obj['revision']['old']}"
+            query = f"?diff={newrev}&oldid={oldrev}"
 
         if obj['minor']:
             flag += "M"
 
-        if obj['bot']:
+        if is_bot:
             flag += "B"
 
         url = f"{obj['server_url']}{obj['server_script_path']}/index.php{query}"
 
-    old_length = obj.get('length',{}).get('old',None)
-    new_length = obj.get('length',{}).get('new',None)
     if old_length is not None and new_length is not None:
         diff_length = new_length - old_length
         if diff_length < -500:
@@ -75,60 +94,57 @@ def format_data(obj):
         f" {bold(diff_length)} {color(comment,'cyan')}"
 
 
-class RCForwarder:
-    matrix_client = None
-    def __init__(self,matrix_client,room):
-        print("init rcforwarder")
-        super().__init__()
-        self.matrix_client = matrix_client
-        self.room = room
+async def forward_news(client, room, message_obj):
+    if not client:
+        raise Exception("matrix_client must be set")
+    html_message = format_data(message_obj)
+    print(html_message)
+    await client.room_send(
+        room,
+        message_type="m.room.message",
+        content={
+            "body": "message",
+            "format": "org.matrix.custom.html",
+            "formatted_body":html_message,
+            "msgtype": "m.notice",
+            }
+        )
 
-    def connection_made(self, transport):
-        print(f"connection made from {transport}")
-        self.transport = transport
+async def read_state(statepath = "state.json" ) -> dict:
+    try:
+        async with aiof.open(statepath, 'r+') as f:
+            return json.loads( await f.read()) # contains 'seen': [ seen_id1, seen_id2]
+    except:
+        print(f"Cannot load contents of {statepath} as json object, falling back to empty")
+        return { "seen": [] } # we start with an empty object
 
-    def datagram_received(self, data, addr):
-        print("Data received")
-        data = data.decode().strip()
-
-        for line in data.split("\n"):
-            try:
-                obj = json.loads(line)
-                print(obj)
-                asyncio.create_task(
-                    self.forward_news(self.matrix_client,self.room,obj)
-                )
-                self.transport.sendto("ok".encode(), addr)
-            except Exception as e:
-                print(e)
-                self.transport.sendto("not ok".encode(), addr)
-
-    def connection_lost(self,transport):
-        pass
+async def write_state(state,statepath = "state.json"):
+    async with aiof.open(statepath, 'w+') as f:
+        await f.write( json.dumps(state))
 
 
-    async def forward_news(self,client, room, message_obj):
-        if not client:
-            raise Exception("matrix_client must be set")
-        html_message = format_data(message_obj)
-        print(html_message)
-        await client.room_send(
-            room,
-            message_type="m.room.message",
-            content={
-                "body": "message",
-                "format": "org.matrix.custom.html",
-                "formatted_body":html_message,
-                "msgtype": "m.notice",
-                }
-            )
+async def fetch_changes():
+    async with aiohttp.ClientSession() as session:
+        #|patrolled
+        async with session.get('https://nixos.wiki/api.php?action=query&list=recentchanges&format=json&rcprop=user|comment|flags|title|sizes|loginfo|ids|revision') as response:
+            return await response.json()
 
-async def check_recent_changes(client):
+def get_new_rcs(rcs,state):
+    for rc in rcs:
+        if rc['rcid'] not in state['seen']:
+            yield rc
+
+async def check_recent_changes(client,room):
     while True:
         print("check recent changes")
-        print(client)
+        resp = await fetch_changes()
+        rcs = resp['query']['recentchanges']
+        print(rcs)
+        state = await read_state()
+        new_rcs = get_new_rcs(rcs,state)
+        for rc in new_rcs:
+            await forward_news(client,room,rc)
         await asyncio.sleep(10)
-        await forward_news(
 
 async def main():
     args = docopt(__doc__)
@@ -138,24 +154,18 @@ async def main():
     with open(config) as config_file:
         config = json.load(config_file)
 
-    #loop = asyncio.get_running_loop()
-
     #Login
     print("login")
     client = AsyncClient(config["server"], config["mxid"])
     print(await client.login(config["password"]))
-    #loop.run_until_complete(server)
     print("create listener")
+
     #transport, protocol = await loop.create_datagram_endpoint(
     #    lambda: RCForwarder(client,config["room"]),
     #    local_addr=('0.0.0.0', 5006))
 
-    #server.matrix_client = client
-    #loop.run_forever()
-    #while True:
-    #    await asyncio.sleep(1)
-    asyncio.create_task(check_recent_changes(client))
-
+    # with that we run check_recent_changes and sync_forever in parallel
+    asyncio.create_task(check_recent_changes(client,config['room']))
     await client.sync_forever(timeout=30000)
 
 
