31b657d486f223ab0d651249508cd89631be1cc3
Jörg Thalheim <joerg@thalheim.io>
2021-06-12 12:54:37 +0200

    add setuptools

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+
+from setuptools import find_packages, setup
+
+setup(
+    name="mediawiki-matrix-bot",
+    version="0.0.1",
+    description="Matrix bot that publishes mediawiki recent changes",
+    author="makefu",
+    author_email="github@syntax-fehler.de",
+    url="https://github.com/https://github.com/nix-community/mediawiki-matrix-bot",
+    license="MIT",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "mediawiki-matrix-bot = mediawiki_matrix_bot:main",
+        ]
+    },
+    extras_require={"dev": ["mypy"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
